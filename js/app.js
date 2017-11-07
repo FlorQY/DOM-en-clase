6d72ae9b2c07e66a0c4507e7a295fcc0f605f259
Mily95 <flor_mili.95@hotmail.com>
2017-11-07 11:36:27 -0500

    Agregando texto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 window.onload= function() {
   var board = document.querySelector(".board-js");
 
-  board.addEventListener("click",function(event){
-    event.target.style.backgroundColor="yellow";
-    event.target.style.border="5px solid green";
-  });
+  board.addEventListener("click", addX);
 };
+   var centinel=true;
+
+   function addX(event) {
+    if(centinel)
+      event.target.textContent ="X";
+
+    else
+      event.target.textContent = "O";
+
+
+    centinel=!centinel;
+  }
